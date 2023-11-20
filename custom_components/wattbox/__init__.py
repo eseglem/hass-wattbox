9f42f77b474ed96d6f317897d7a48b7888a067b3
Erik Seglem <erik.seglem@gmail.com>
2023-11-20 20:57:14 +0000

    Update pywattbox version.

--- custom_components/wattbox/__init__.py
+++ custom_components/wattbox/__init__.py
@@ -44,7 +44,7 @@ from .const import (
     TOPIC_UPDATE,
 )
 
-REQUIREMENTS: Final[List[str]] = ["pywattbox>=0.7.1"]
+REQUIREMENTS: Final[List[str]] = ["pywattbox>=0.7.2"]
 
 _LOGGER = logging.getLogger(__name__)
 
